4a8a16ae6aed9ee92ac215636f13d2ed7bbd3339
VasylievKostia <80004260+VasylievKostia@users.noreply.github.com>
2022-01-18 00:55:55 +0200

    doubleclick bnt fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { interval, Subject } from 'rxjs'
-import {takeUntil} from 'rxjs/operators'
-import {RenderTime} from './components/RenderTime'
+import React, { useState, useEffect, useRef } from 'react';
+
+import {
+  buffer,
+  debounceTime,
+  filter,
+  fromEvent,
+  interval,
+  map,
+  Subject,
+  takeUntil,
+} from "rxjs";
+
+import { RenderTime } from './components/RenderTime'
+
 
 function App() {
   const [time, setTime] = useState(0)
   const [stopwatchStatus, setStopwatchStatus] = useState(false)
   const [isWait, setIsWait] = useState(false)
-
-
+  const waitBtnRef = useRef(null)
+  
+// timer switcher
   useEffect(() => {
     const unsubscribe = new Subject()
     interval(10)
@@ -24,11 +36,27 @@ function App() {
       unsubscribe.complete()
     }
   }, [stopwatchStatus])
-  
-  const handleStart = () => {
-    if (isWait) {
-      setStopwatchStatus(true)
-      return
+
+ //double click handler
+  useEffect(() => {
+    const waitBtn$ = fromEvent(waitBtnRef.current, 'click')
+    const delay$ = waitBtn$.pipe(debounceTime(300))
+    const $subscribeWaitBtn = waitBtn$
+      .pipe(
+        buffer(delay$),
+        map((event) => event.length),
+        filter((clickCount) => clickCount === 2)
+    )
+      .subscribe(() => {
+          handleWait()
+      })
+      return () => $subscribeWaitBtn.unsubscribe()      
+  },[])
+ 
+ const handleStart = () => {
+   if (isWait) {
+     setStopwatchStatus(true)
+     return
     }
     setStopwatchStatus(prev => !prev)
   }
@@ -45,15 +73,15 @@ function App() {
     setStopwatchStatus(false)
     setIsWait(true)
   }
-
+  
   return (
     <div className="App">
       <h1 className='header'>Stopwatch</h1>
       <RenderTime time={time} />
         <div className="buttons">
-          <button onDoubleClick={handleWait} className='stopwatch__btn btn'>Wait</button>
+          <button ref={waitBtnRef} className='stopwatch__btn btn'>Wait</button>
           {stopwatchStatus ? ( <button onClick={handleStop} className='stopwatch__stopBtn btn'>Stop</button>) : (<button onClick={handleStart} className='stopwatch__startBtn btn'>Start</button>) }  
-          <button onClick={handleReset} className='stopwatch__btn btn'>Reset</button>
+        <button onClick={handleReset} className='stopwatch__btn btn'>Reset</button>
         </div>
     </div>
   );
